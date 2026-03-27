d273ed971f87818a126b84bbe445a20fa2925fa6
paolaandreapd42-dot <paolaandreapd42@gmail.com>
2026-03-27 14:26:28 -0500

    actualizacion_2703

--- 0_inicio.py
+++ 0_inicio.py
@@ -13,7 +13,7 @@ icono = Image.open("Icono_diabetes.png")
 st.set_page_config(page_title="Mi APP_Diabetes", page_icon=icono, layout="wide")
 
 ##Agregar titulo y descripcion
-st.sidebar.header("La diabetes no siempre se siente, pero siempre cuenta: cada decisión diaria puede marcar la diferencia entre el riesgo y el bienestar.")
+st.sidebar.header("La diabetes no siempre se siente, pero siempre cuenta. Cada decisión diaria puede marcar la diferencia entre el riesgo y el bienestar.")
 st.markdown("<h1 style='color:blue;'>Predicción de la Diabetes</h1>", unsafe_allow_html=True)
 ##st.write("La diabetes es una condición crónica que afecta la forma en que el cuerpo utiliza la glucosa, su principal fuente de energía. Existen diferentes tipos de diabetes, y cada uno requiere cuidados especiales para mantener una buena calidad de vida. El objetivo de esta página es brindar información básica, fomentar la conciencia sobre la importancia de la prevención y promover hábitos saludables que ayuden a controlar la enfermedad.")
 
